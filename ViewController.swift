fbf21fc88ba793f1efd021ee35177cca5cc926e5
dpill <a20141105059@mac05.local>
2016-06-22 18:06:42 +0800

    通过今天的学习，做出了加减乘除的基本运算，有几次出错完全不知道为什么，重新做一份又没错了，这份程序就是多次试验后的幸运儿。

---
+++ ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  czh
+//
+//  Created by 20141105059c on 16/6/22.
+//  Copyright © 2016年 20141105059c. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var x: UITextField!
+    @IBOutlet weak var y: UITextField!
+    @IBOutlet weak var z: UITextField!
+    @IBAction func multiply(sender: AnyObject) {
+        var temp=0
+        temp=(Int)(x.text!)!*(Int)(y.text!)!
+        z.text="\(temp)"
+    }
+    @IBAction func add(sender: AnyObject) {
+        var temp=0
+        temp=(Int)(x.text!)!+(Int)(y.text!)!
+        z.text="\(temp)"
+    }
+    @IBAction func subtract(sender: AnyObject) {
+        var temp=0
+        temp=(Int)(x.text!)!-(Int)(y.text!)!
+        z.text="\(temp)"
+    }
+    @IBAction func divide(sender: AnyObject) {
+        var temp=0
+        temp=(Int)(x.text!)!/(Int)(y.text!)!
+        z.text="\(temp)"
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
